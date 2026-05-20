991728a41a015429517e26525c1cad2672a64036
DHUDAMrakesh <147386623+DHUDAMrakesh@users.noreply.github.com>
2026-05-20 13:28:16 +0530

    package.json fixed

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -4,8 +4,8 @@ import { PrismaNeon } from "@prisma/neon";
 import { neonConfig } from "@neondatabase/serverless";
 import ws from "ws";
 neonConfig.webSocketConstructor = ws;
-const connnectionString = process.env.DATABASE_URL;
-const adapter = new PrismaNeon({ connnectionString });
+const connectionString = process.env.DATABASE_URL;
+const adapter = new PrismaNeon({ connectionString });
 const prisma = global.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
