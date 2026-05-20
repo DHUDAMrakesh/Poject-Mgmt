8623d95dcaa163689f9a3ec272d39cc1054fe22a
DHUDAMrakesh <147386623+DHUDAMrakesh@users.noreply.github.com>
2026-05-20 13:37:24 +0530

    error fixed

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
@@ -9,7 +11,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
-dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Server is Live");
