90516bb88e1ac8d696cfcb9f8bf462c83679c6f0
DHUDAMrakesh <147386623+DHUDAMrakesh@users.noreply.github.com>
2026-05-26 11:23:47 +0530

    fix cors preflight

--- server/server.js
+++ server/server.js
@@ -37,7 +37,6 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
-app.options("*", cors());
 app.post(
   "/api/webhooks/clerk",
   express.raw({ type: "application/json" }),
