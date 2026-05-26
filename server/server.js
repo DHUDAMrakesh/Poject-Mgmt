ea404839f12253e428dfb6c4a82f520ec101358f
DHUDAMrakesh <147386623+DHUDAMrakesh@users.noreply.github.com>
2026-05-26 15:43:33 +0530

    fixed

--- server/server.js
+++ server/server.js
@@ -41,6 +41,27 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
+app.use((req, res, next) => {
+  const origin = req.get("origin");
+
+  if (isAllowedOrigin(origin)) {
+    res.header("Access-Control-Allow-Origin", origin || "*");
+    res.header("Vary", "Origin");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET,POST,PUT,DELETE,PATCH,OPTIONS",
+    );
+    res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  }
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 app.use(cors(corsOptions));
 app.options(/.*/, cors(corsOptions));
 app.post(
